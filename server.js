bd2ef35ae56657f57ec5ba660a2fb5d2130f38af
Donovan Preston <donovanpreston@gmail.com>
2014-03-14 14:53:12 -0700

    Implement digging

--- server.js
+++ server.js
@@ -36,16 +36,21 @@ engine.attach(http).on('connection', function (socket) {
         //console.log("ui said", pat, data);
         socket.send(JSON.stringify({pattern: pat, data: data}));
       }
+      var pos = random(16) + "," + random(16);
+      socket.send(JSON.stringify(
+        {pattern: 'login', data: {name: msg.data.name, pos: pos}}));
       vat.spawn(act("player"), socket.id, ui_func);
       room('join', {
         address: socket.id,
         name: msg.data.name,
-        pos: "" + random(16) + "," + random(16)});
+        pos: "" + pos});
       console.log("connected");
     } else if (msg.pattern === "msg") {
       room("msg", {address: socket.id, msg: msg.data});
     } else if (msg.pattern === "go") {
       room("go", {address: socket.id, pos: msg.data});
+    } else if (msg.pattern === "dig") {
+      room("dig", msg.data);
     }
   });
   socket.on('close', function () {
